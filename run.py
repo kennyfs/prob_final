ff06836af288c93710631a48abb5b5ccfa13bdef
kenny <kenny.kuo.fs@gmail.com>
2024-06-04 00:17:22 +0800

    run: sort by c,a,b

--- run.py
+++ run.py
@@ -31,6 +31,11 @@ def add_noise(seed, training_data):
         if rng.random() < 0.5:
             training_data[i], training_data[i-1] = training_data[i-1], training_data[i]
     return training_data
+def sort_by_c_a_b(seed, training_data):
+    training_data=training_data.tolist()
+    training_data.sort(key=lambda x: x[4:6]+x[0:2]+x[2:4])
+    training_data = add_noise(seed, training_data)
+    return torch.tensor(training_data, dtype=torch.long)
 def get_config(seed, task, initialization):
     C = CN()
 
@@ -114,6 +119,6 @@ if __name__ == '__main__':
     dataset_seed = int(sys.argv[4])
     print(f"get dataset seed {dataset_seed}")
 
-    stop_iteration = run(seed, dataset_seed, task, initialization, shuffle_within_batch)
-    with open(f"result-{task}-{initialization}.csv", "a") as f:
+    stop_iteration = run(seed, dataset_seed, task, initialization, sort_by_c_a_b)
+    with open(f"result-sort_by_c_a_b-{task}.csv", "a") as f:
         f.write(f"{dataset_seed}, {stop_iteration}\n")
